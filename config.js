60987b6e6ea7311b017abbd57d599263834ffce1
prasad19881988-creator <prasad19881988@gmail.com>
2026-08-22 01:01:17 +0530

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// One-time setup: paste your Supabase project values here after creating the project.
-// These two values are safe to use in a browser when your database RLS policies are enabled.
-window.SS_CONFIG = {
-  SUPABASE_URL: "PASTE_SUPABASE_PROJECT_URL_HERE",
-  SUPABASE_ANON_KEY: "PASTE_SUPABASE_ANON_KEY_HERE"
-};
